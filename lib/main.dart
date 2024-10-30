e0dfea9e28026d1f23b3068c279a4b56b523e89e
hedqu033 <hedqu033@d.umn.edu>
2024-10-30 04:53:36 -0500

    framework for the calander app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-//test
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
