806d9b8739ad5b8a6f48dd082f631b94a9dd0edc
Randy Zhagui <zhagu009@d.umn.edu>
2024-10-30 15:29:27 -0500

    Implemented CalorieNinjas API

--- lib/model.dart
+++ lib/model.dart
@@ -1,9 +1,35 @@
-class AppModel {
-  //sample method to fetch or proccess data
-  Future<String> fetchData() async {
-    //simulating a data fetch or api call
-    await Future.delayed(Duration(seconds: 2));
-    return "Hello from Model!";
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-  }
+class AppModel {
+  final String apiKey = 'Your-API-Key'; 
+  //sample method to fetch or proccess data
+  Future<String> fetchData(String query) async {
+    //simulating a data fetch or api call
+    final response = await http.get(
+      Uri.parse("https://api.calorieninjas.com/v1/nutrition?query=$query"),
+      headers:{"X-Api-Key": apiKey}
+      );
+
+      if (response.statusCode == 200){
+        //parsing JSON response to get nutrient information
+        final data = json.decode(response.body);
+
+        //extracting relevant nutrient information for display 
+        String nutrientSummary = data['items'].map((item){
+          return "${item['name']}: ${item['calories']} kcal";
+        }).join("\n");
+        return nutrientSummary;
+      } else {
+          //handle API error response
+          return "Failed to load nutrient data";  
+         }
+         
+          
+             
+    }
+    // await Future.delayed(Duration(seconds: 2));
+    // return "Nutrient Summary: !";
 }
+
