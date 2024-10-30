d6625ec771b1fe734d61f3d43b1304e2021da6c2
Randy Zhagui <zhagu009@d.umn.edu>
2024-10-30 14:09:19 -0500

    Implemented main app strcuture with MVP pattern
    created HomeScreen to mange nutrient data

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,49 @@ import 'package:flutter/material.dart';
 import 'model.dart';
 import 'presenter.dart';
 import 'view.dart';
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // Initialize Model and Presenter
-    AppModel model = AppModel();
-    AppPresenter presenter;
-
     return MaterialApp(
-      home: Builder(
-        builder: (context) {
-          // Connect the Presenter to the View
-          presenter = AppPresenter(model, (data) {
-            // Display the data as a SnackBar for simplicity
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(data)),
-            );
-          });
-          return AppView(presenter);
-        },
+      title: 'Tracking Your Nutrients',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String nutrientData = "No nutrient data loaded";
+
+  @override
+  Widget build(BuildContext context) {
+    // Initialize the model and presenter here
+    final model = AppModel();
+    final presenter = AppPresenter(model, updateView);
+
+    // Pass `nutrientData` to AppView as required
+    return AppView(presenter, data: nutrientData);
+  }
+
+  void updateView(String newData) {
+    setState(() {
+      nutrientData = newData;
+    });
   }
 }
