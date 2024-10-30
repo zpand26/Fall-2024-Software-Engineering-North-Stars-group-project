513a8eaa8d6077d10147dce5ace8e93a31053df6
Randy Zhagui <zhagu009@d.umn.edu>
2024-10-30 15:31:09 -0500

    added click button for food item entry

--- lib/view.dart
+++ lib/view.dart
@@ -3,19 +3,33 @@ import 'presenter.dart';
 
 class AppView extends StatelessWidget {
   final AppPresenter presenter;
+  final String data;
 
-  AppView(this.presenter);
+  AppView(this.presenter, {required this.data});
+
+  final TextEditingController _queryController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MVP Example'),
+        title: Text('Nutrient Tracker'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => presenter.loadData(),
-          child: Text('Load Data'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _queryController,
+              decoration: InputDecoration(hintText: 'Enter food items'),
+            ),
+            ElevatedButton(
+              onPressed: () => presenter.loadData(_queryController.text), 
+              child: Text('Showing Nutrient Summary'),
+              ),
+              SizedBox(height: 20), 
+              Text(data, style: TextStyle(fontSize: 18)),
+          ],
         ),
       ),
     );
