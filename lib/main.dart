868398538fb59d6186d672e6ad6af1874fcfd8cd
kue00020 <kue00020@d.umn.edu>
2024-10-30 13:56:48 -0500

    developing filter feature through strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model.dart';
-import 'presenter.dart';
-import 'view.dart';
+import 'meal_filter_feature.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,23 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Initialize Model and Presenter
-    AppModel model = AppModel();
-    AppPresenter presenter;
-
     return MaterialApp(
-      home: Builder(
-        builder: (context) {
-          // Connect the Presenter to the View
-          presenter = AppPresenter(model, (data) {
-            // Display the data as a SnackBar for simplicity
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(data)),
-            );
-          });
-          return AppView(presenter);
-        },
+      title: 'Meal Filter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MealFilterFeature(),
     );
   }
 }
