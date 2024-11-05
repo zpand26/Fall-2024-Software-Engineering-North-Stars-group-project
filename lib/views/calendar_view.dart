96d115df0c934d5d19942b2b3fd235add9ba9079
hedqu033 <hedqu033@d.umn.edu>
2024-11-05 14:44:45 -0600

    commmented out a file that was causing issues and merged calander

--- lib/views/calendar_view.dart
+++ lib/views/calendar_view.dart
@@ -3,7 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:firebase_database/firebase_database.dart';
+//import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import '../presenters/calendar_presenter.dart';
