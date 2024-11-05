1bfbf09cb4c4d5756a558a88b9b4e0d1a6238f79
hedqu033 <hedqu033@d.umn.edu>
2024-11-05 14:30:20 -0600

    Revert "framework for the calander app"
    This reverts commit e0dfea9e28026d1f23b3068c279a4b56b523e89e.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:table_calendar/table_calendar.dart';
+//test
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
---
+++ lib/tableCalendar.dart
@@ -0,0 +1,5 @@
+class TableCalendar {
+firstDay: DateTime.utc(2010, 10, 16),
+lastDay: DateTime.utc(2030, 3, 14),
+focusedDay: DateTime.now(),
+}
