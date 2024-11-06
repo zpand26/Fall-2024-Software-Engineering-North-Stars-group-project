f75635f8def2a1a3e4c9b74bd774a4f7f4036f8d
hedqu033 <hedqu033@d.umn.edu>
2024-11-06 15:46:44 -0600

    redid the calander app, allows for events to be added and can be viewed on the calander

--- lib/views/calendar_view.dart
+++ lib/views/calendar_view.dart
@@ -1,59 +1,155 @@
-// Copyright 2019 Aleksander Woźniak
-// SPDX-License-Identifier: Apache-2.0
-
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
-//import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-import '../presenters/calendar_presenter.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TableCalendar Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: StartPage(),
+      home: CalendarScreen(),
     );
   }
 }
 
-class StartPage extends StatefulWidget {
+class CalendarScreen extends StatefulWidget {
   @override
-  _StartPageState createState() => _StartPageState();
+  _CalendarScreenState createState() => _CalendarScreenState();
 }
 
-class _StartPageState extends State<StartPage> {
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  Map<DateTime, List<String>> _events = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TableCalendar Example'),
+        title: Text('Calendar'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text('Basics'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableBasicsExample()),
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            const SizedBox(height: 20.0),
-          ],
-        ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // update `_focusedDay` here as well
+              });
+            },
+            eventLoader: (day) {
+              return _events[day] ?? [];
+            },
+          ),
+          ..._getEventsForDay(_selectedDay).map((event) => ListTile(
+            title: Text(event),
+          )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showAddEventDialog(),
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  List<String> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
+  }
+
+  void _showAddEventDialog() {
+    TextEditingController eventController1 = TextEditingController();
+    TextEditingController eventController2 = TextEditingController();
+    TextEditingController eventController3 = TextEditingController();
+    TextEditingController eventController4 = TextEditingController();
+    TextEditingController eventController5 = TextEditingController();
+    TextEditingController eventController6 = TextEditingController();
+    TextEditingController eventController7 = TextEditingController();
+    TextEditingController eventController8 = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Event'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: eventController1,
+                  decoration: InputDecoration(hintText: 'Calories'),
+                ),
+                TextField(
+                  controller: eventController2,
+                  decoration: InputDecoration(hintText: 'Total Fat'),
+                ),
+                TextField(
+                  controller: eventController3,
+                  decoration: InputDecoration(hintText: 'Cholestorol'),
+                ),
+                TextField(
+                  controller: eventController4,
+                  decoration: InputDecoration(hintText: 'Sodium'),
+                ),
+                TextField(
+                  controller: eventController5,
+                  decoration: InputDecoration(hintText: 'Total Carbohydrate'),
+                ),
+                TextField(
+                  controller: eventController6,
+                  decoration: InputDecoration(hintText: 'Fiber'),
+                ),
+                TextField(
+                  controller: eventController7,
+                  decoration: InputDecoration(hintText: 'Total Sugar'),
+                ),
+                TextField(
+                  controller: eventController8,
+                  decoration: InputDecoration(hintText: 'Protein'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (eventController1.text.isNotEmpty || eventController2.text.isNotEmpty) {
+                  _addEvent('${eventController1.text} ${eventController2.text}');
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addEvent(String event) {
+    setState(() {
+      if (_events[_selectedDay] != null) {
+        _events[_selectedDay]!.add(event);
+      } else {
+        _events[_selectedDay] = [event];
+      }
+    });
   }
 }
