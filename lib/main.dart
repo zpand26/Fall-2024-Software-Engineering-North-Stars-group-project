3857c841b25342fc0daa81b9b28be10ac60a849f
SamHovda <hovda040@d.umn.edu>
2024-11-07 15:04:39 -0600

    deleted unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:north_stars/models/nutrient_tracking_model.dart';
 import 'package:north_stars/views/login_page_view.dart';
-import 'models/notification_model.dart';
-import 'presenters/notification_presenter.dart';
-import 'views/notification_view.dart';
-import 'notification/home.dart';
 import 'notification/initialization.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:north_stars/models/data_entry_for_day_model.dart';
 import 'models/calorie_tracker_model.dart';
-import 'presenters/calorie_tracker_presenter.dart';
-import 'views/home_page_view.dart';
-import 'views/notification_view.dart';
-import 'views/notification_home.dart';
-import 'notification/initialization.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:north_stars/models/notification_model.dart';
 //import 'package:north_stars/presenters/notification_presenter.dart';
 
 
