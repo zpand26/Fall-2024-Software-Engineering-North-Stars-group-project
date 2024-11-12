797ab142c3551f5f1287e7438b93adb877ce3150
SamHovda <hovda040@d.umn.edu>
2024-11-12 13:42:06 -0600

    updated path of calorie adding

--- lib/models/calorie_tracker_model.dart
+++ lib/models/calorie_tracker_model.dart
@@ -1,31 +1,61 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CalorieTrackerModel {
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //get current users ID
+  String get userId => _auth.currentUser?.uid ?? '';
 
 
   //add solid calories to firebase
-  Future<void> addSolidCalories(int calorie) async{
-    await _firestore.collection('solidCalories').add({'calorie': calorie, 'timestamp': DateTime.now()});
+  Future<void> addSolidCalories(int calorie) async {
+    if (userId.isNotEmpty) {
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('solidCalories')
+          .add({'calorie': calorie, 'timestamp': DateTime.now()});
+    }
   }
 
   //add liquid calories to firebase
-  Future<void> addLiquidCalories(int calorie) async{
-    await _firestore.collection('liquidCalories').add({'calorie': calorie, 'timestamp': DateTime.now()});
+  Future<void> addLiquidCalories(int calorie) async {
+    if (userId.isNotEmpty) {
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('liquidCalories')
+          .add({'calorie': calorie, 'timestamp': DateTime.now()});
+    }
   }
-
   //get total solid calories
   Future<int> getTotalSolidCalories() async {
-    final querySnapshot = await _firestore.collection('solidCalories').get();
-    return querySnapshot.docs.fold<int>(0, (int total, doc) => total + (doc['calorie'] as int));
+    if (userId.isEmpty) return 0;
+
+    final querySnapshot = await _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('solidCalories')
+        .get();
+    return querySnapshot.docs.fold<int>(
+        0, (total, doc) => total + (doc['calorie'] as int));
   }
 
   //get total liquid calories
   Future<int> getTotalLiquidCalories() async {
-    final querySnapshot = await _firestore.collection('liquidCalories').get();
-    return querySnapshot.docs.fold<int>(0, (int total, doc) => total + (doc['calorie'] as int));
+    if (userId.isEmpty) return 0;
+
+    final querySnapshot = await _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('liquidCalories')
+        .get();
+    return querySnapshot.docs.fold<int>(
+        0, (total, doc) => total + (doc['calorie'] as int));
   }
 
   //get the total calorie count
@@ -34,5 +64,4 @@ class CalorieTrackerModel {
     int totalSolid = await getTotalSolidCalories();
     return totalLiquid + totalSolid;
   }
-
 }
